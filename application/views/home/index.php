424c98d40dbe9392c2ffd0e7f52341f66c8867e7
mks110 <55471179+mks110@users.noreply.github.com>
2020-03-16 13:33:44 +0700

    as

---
+++ application/views/home/index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+	foreach ($seo->result_array() as $value) {
+		$tittle = $value['tittle'];
+		$keyword = $value['keyword'];
+		$description = $value['description'];
+	}
+	?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keyword" content="<?php echo $keyword;?>">
+    <meta name="description" content="<?php echo $description;?>">
+    <meta name="author" content="">
+    <title>Home | <?php echo $tittle;?></title>
+    <link href="<?php echo base_url();?>asset/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>asset/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>asset/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>asset/css/price-range.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>asset/css/animate.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>asset/css/main.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>asset/css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url();?>asset/js/html5shiv.js"></script>
+    <script src="<?php echo base_url();?>asset/js/respond.min.js"></script>
+    <![endif]-->       
+    
+</head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<?php
+							foreach ($logo->result_array() as $value) {
+								$logo = $value['gambar'];
+							}
+
+							?>
+							<a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>images/logo/<?php echo $logo;?>" alt="Adriano MX Online Shop" /></a>
+						</div>
+						<div class="btn-group pull-right">
+							
+							
+							
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	
+		<div class="header-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="<?php echo base_url();?>" class="active">Home</a></li>
+								
+							
+								
+								
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</header>
+
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						
+					</br>
+
+						<div class="brands_products"><!--Jasa Pengiriman-->
+							<h2>Pengiriman</h2>
+							<div class="brands-name">
+								<ul class="nav nav-pills nav-stacked">
+									<?php
+									foreach ($jasapengiriman->result_array() as $value) { ?>
+									<li><a href=""> <span class="pull-right"></span>
+										<img src="<?php echo base_url();?>images/jasapengiriman/<?php echo $value['gambar'];?>">
+										</a>
+									</li>
+									
+									<?php
+									}
+									?>
+									
+									
+								</ul>
+							</div>
+						</div><!--/Jasa Pengiriman-->
+						
+						
+						
+						
+					
+					</div>
+				</div>
+				
+				<div class="col-sm-9 padding-right">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Features Producs</h2>
+						
+						<?php
+						foreach ($produk->result_array() as $value) { ?>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-center">
+											<img src="<?php echo base_url();?>images/produk/<?php echo $value['gambar'];?>" alt="" />
+											<h2><?php echo $value['harga'];?></h2>
+											<p><?php echo $value['kode_produk'];?></p>
+											<a href="<?php echo base_url();?>home/produk/<?php echo $value['id_produk'];?>"><p> <?php echo $value['nama_produk'];?></p></a>
+											<a href="<?php echo base_url();?>home/keranjang/<?php echo $value['id_produk'];?>" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2><?php echo $value['harga'];?></h2>
+												<p><?php echo $value['kode_produk'];?></p>
+												<a href="<?php echo base_url();?>home/produk/<?php echo $value['id_produk'];?>"><p> <?php echo $value['nama_produk'];?></p></a>
+												<a href="<?php echo base_url();?>home/keranjang/<?php echo $value['id_produk'];?>" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+											</div>
+										</div>
+								</div>
+							</div>
+						</div>
+						<?php
+						}
+						?>
+						
+						
+						
+						
+						
+						
+						
+					</div><!--features_items-->
+					
+					
+					
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+	
+
+  
+    <script src="<?php echo base_url();?>asset/js/jquery.js"></script>
+	<script src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url();?>asset/js/jquery.scrollUp.min.js"></script>
+	<script src="<?php echo base_url();?>asset/js/price-range.js"></script>
+    <script src="<?php echo base_url();?>asset/js/jquery.prettyPhoto.js"></script>
+    <script src="<?php echo base_url();?>asset/js/main.js"></script>
+</body>
+</html>
