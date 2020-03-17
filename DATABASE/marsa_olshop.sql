f5f5879ceeff16af94c737114957098a3dd7dddc
mks110 <55471179+mks110@users.noreply.github.com>
2020-03-17 23:17:42 +0700

    fell
    aw

---
+++ DATABASE/marsa_olshop.sql
@@ -0,0 +1,296 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2020 at 05:15 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marsa_olshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` bigint(15) NOT NULL,
+  `hak_akses` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `nama_admin`, `username`, `password`, `email`, `phone`, `hak_akses`) VALUES
+(1, 'Admin Marsa', 'adminweb', '566ecb0b5565e0899f99491fff1b265f', 'adminadriano@gmail.com', 85648263456, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_brand`
+--
+
+CREATE TABLE `tbl_brand` (
+  `id_brand` int(11) NOT NULL,
+  `nama_brand` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_brand`
+--
+
+INSERT INTO `tbl_brand` (`id_brand`, `nama_brand`) VALUES
+(1, 'Brand Satu'),
+(2, 'Brand Dua'),
+(3, 'Brand Tiga'),
+(4, 'Brand Empat'),
+(5, 'Brand Lima'),
+(6, 'Brand Enam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_galeri`
+--
+
+CREATE TABLE `tbl_galeri` (
+  `id_galeri` int(11) NOT NULL,
+  `nama_galeri` varchar(200) NOT NULL,
+  `gambar` varchar(200) NOT NULL,
+  `kategorigaleri_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_galeri`
+--
+
+INSERT INTO `tbl_galeri` (`id_galeri`, `nama_galeri`, `gambar`, `kategorigaleri_id`) VALUES
+(1, 'Jersea Motor', 'dec10698e402e54bbb65e199d1612127.gif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_jasapengiriman`
+--
+
+CREATE TABLE `tbl_jasapengiriman` (
+  `id_jasapengiriman` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `gambar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_jasapengiriman`
+--
+
+INSERT INTO `tbl_jasapengiriman` (`id_jasapengiriman`, `nama`, `gambar`) VALUES
+(1, 'JNE', '9161e6bd8ac2a57a7c9450bf84dee661.png'),
+(2, 'TIKI', 'e6cb91a9459bc8af1f9685f947e1ffef.png'),
+(3, 'ESL Express', 'cd1d63e790e558c44d0f538b51a72830.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Sepatu'),
+(2, 'Sarung Tangan'),
+(3, 'Motorcros'),
+(4, 'Kacamata'),
+(5, 'Jersey'),
+(6, 'Helm'),
+(7, 'Dewasa'),
+(8, 'Celana'),
+(9, 'Anak-anak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_logo`
+--
+
+CREATE TABLE `tbl_logo` (
+  `id_logo` int(11) NOT NULL,
+  `gambar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_logo`
+--
+
+INSERT INTO `tbl_logo` (`id_logo`, `gambar`) VALUES
+(1, 'e27504c5cfbd695105b1cd7a6af9ba78.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(11) NOT NULL,
+  `kode_produk` varchar(20) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga` bigint(15) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(200) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `kode_produk`, `nama_produk`, `harga`, `stok`, `deskripsi`, `gambar`, `kategori_id`, `brand_id`) VALUES
+(1, 'AMX00001', 'Easy Polo Black Edition', 56000, 10, 'Lorem Ipsum Doler', '11c226c3c77b62dccaa471601fe8a26e.jpg', 3, 1),
+(2, 'AMX00002', 'Easy Polo Black Edition', 56000, 20, 'Lorem Ipsum doller', 'f5b09c1b010be586adf31ce8b1206392.jpg', 4, 2),
+(3, 'AMX00003', 'Produk Ketiga', 56000, 2, 'Lorem Ipsum Doler', '919d2d896c3cb9e376b150cb0c938f43.jpg', 5, 3),
+(4, 'AMX00004', 'Easy Polo Black Edition', 56000, 10, 'Lorem Ipsum Doller', '36a1b6cfca6908d94e4f860cd16870fc.jpg', 9, 4),
+(5, 'AMX00005', 'Easy Polo Black Edition', 36000, 1, 'Lorem Ipsum Doller', '043c68c05a1635d36ab1033f862d71e6.jpg', 9, 5),
+(6, 'AMX00006', 'Easy Polo Black Edition', 26000, 10, 'Lorem Ipsum doller', '1e4afb3f73034c618a7e0391aa9b0d1a.jpg', 9, 6),
+(7, 'AMX00007', 'Easy Polo Black Edition', 16000, 5, 'Lorem Ipsum Doler', 'd4f7655973e9e358ce3d7d0301039867.jpg', 9, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_seo`
+--
+
+CREATE TABLE `tbl_seo` (
+  `id_seo` int(11) NOT NULL,
+  `tittle` varchar(50) NOT NULL,
+  `keyword` varchar(500) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_seo`
+--
+
+INSERT INTO `tbl_seo` (`id_seo`, `tittle`, `keyword`, `description`) VALUES
+(1, 'Botstrap template', 'Botstrap template', 'Botstrap template');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  ADD PRIMARY KEY (`id_brand`);
+
+--
+-- Indexes for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  ADD PRIMARY KEY (`id_galeri`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_logo`
+--
+ALTER TABLE `tbl_logo`
+  ADD PRIMARY KEY (`id_logo`);
+
+--
+-- Indexes for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `tbl_seo`
+--
+ALTER TABLE `tbl_seo`
+  ADD PRIMARY KEY (`id_seo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_brand`
+--
+ALTER TABLE `tbl_brand`
+  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_logo`
+--
+ALTER TABLE `tbl_logo`
+  MODIFY `id_logo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
